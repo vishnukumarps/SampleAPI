5308316635923d71d4efeb04a7c499305dc24025
Vishnukumarps <vishnukumar28518@gmail.com>
2023-09-10 06:33:11 +0530

    test vishnu

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -20,6 +20,8 @@ namespace SampleAPI.Controllers
 
         [HttpGet(Name = "GetWeatherForecast")]
 
+
+
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
